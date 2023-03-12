ff206c35556ed47c72995cef959aba1347cd446b
leekoby <abc2752@naver.com>
2023-03-12 18:17:37 +0900

    javscript를 Typescript처럼 하는 방법

--- why-ts/sample.js
+++ why-ts/sample.js
@@ -1,6 +1,21 @@
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// sum(10, `20`);
+
+
+//@ts-check
+
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
+ */
 function sum(a, b) {
   return a + b;
 }
 
-sum(10, `20`);
 
+sum(10, `20`)
